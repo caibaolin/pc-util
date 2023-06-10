50466c961cfff9c9845e310aaf92b14594e9cf3d
蔡宝林 <401115406@qq.com>
2023-06-10 17:58:11 +0800

    第一次提交3

--- common/src/commonMain/kotlin/com/roush/common/App.kt
+++ common/src/commonMain/kotlin/com/roush/common/App.kt
@@ -2,6 +2,8 @@ package com.roush.common
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.snapping.SnapFlingBehavior
+import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
